627719800cf1afdcee115e47bf363abb4a7ac432
JatinShimpi <shimpijatin184@gmail.com>
2024-06-14 01:15:46 +0530

    fixed the problem with the formsplitbill comp.

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default function App() {
         <Formsplitbill
           selectedFriend={selectedFriend}
           onSplitbill={handlesplitbill}
+          key={selectedFriend.id}
         />
       )}
     </div>
